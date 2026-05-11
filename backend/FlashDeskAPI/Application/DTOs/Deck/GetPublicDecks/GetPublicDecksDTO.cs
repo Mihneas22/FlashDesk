b9b7c299faaf9a096808a533fd2bfccf0d05cce0
Mihneas22 <mihnea.serban06@gmail.com>
2026-05-11 15:57:37 +0300

    privacy issue3

--- backend/FlashDeskAPI/Application/DTOs/Deck/GetPublicDecks/GetPublicDecksDTO.cs
+++ backend/FlashDeskAPI/Application/DTOs/Deck/GetPublicDecks/GetPublicDecksDTO.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
 
 namespace Application.DTOs.Deck.GetPublicDecks
 {
